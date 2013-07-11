b32bc66311b61fabf0951934364a61a8376755c6
Ubuntu <ubuntu@ip-172-31-20-3.us-west-2.compute.internal>
2013-07-11 01:23:20 +0000

    Second attempt at reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var buffer = fs.readFileSync(infile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8", 0, buffer.length));
 });
 
 var port = process.env.PORT || 5000;
